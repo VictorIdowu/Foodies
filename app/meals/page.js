5c66ae3dba2ca43a9c0b4b759e26736c0ef2756b
VictorIdowu <idsonvictor@gmail.com>
2024-02-21 18:22:00 +0100

    mmm

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,10 +1,25 @@
 import React from "react";
+import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/meals/MealsGrid";
 
 const Meals = () => {
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>Meals</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created {""}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself.</p>
+        <p className={classes.cta}>
+          <Link href={"/meals/share"}>Share your Favoritw Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 };
 
--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import classes from "./page.module.css";
-import ImageSlideshow from "@/components/main-header/images/ImageSlideshow";
+import ImageSlideshow from "@/components/images/ImageSlideshow";
 const Home = () => {
   return (
     <>
