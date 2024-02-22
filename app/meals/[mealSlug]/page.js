c30ec9e8533076b860bbb8c9d8352710c3418c55
VictorIdowu <idsonvictor@gmail.com>
2024-02-22 18:17:27 +0100

    mmm

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,35 @@
+import React from "react";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import Image from "next/image";
+
+const MealsDetails = ({ params }) => {
+  const meal = getMeal(params.mealSlug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br/>");
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
+  );
+};
+
+export default MealsDetails;
